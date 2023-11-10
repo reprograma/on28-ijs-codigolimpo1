ab695ccdc2b71430e26c94d619d89d8e48c9de19
Heloiza Mendes <helocursos@gmail.com>
2023-11-10 01:57:13 -0300

    Clean code - heloiza mendes

--- exercicios/para-casa/entregas/exemplo-nome-sobrenome/resolucao.js
+++ exercicios/para-casa/entregas/exemplo-nome-sobrenome/resolucao.js
@@ -6,18 +6,14 @@ Rental.prototype.statement = function (customer) {
     F002: { title: "Trois Couleurs: Bleu", code: "regular" },
     F003: { title: "Cars 2", code: "infantil" },
     F004: { title: "Latest Hit Release", code: "lançamento" },
-    //Pode alterar os filmes se você quiser :D
   };
   let totalAmount = 0;
   let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
   for (let r of customer.rentals) {
-    // rentals = [{movieID: string, days: number, title: string }, {} ]
     let movie = movies[r.movieID];
     let thisAmount = 0;
 
-    // determine amount for each movie
-    // determina o custo para cada filme
     switch (movie.code) {
       case "regular":
         thisAmount = 2;
@@ -36,16 +32,13 @@ Rental.prototype.statement = function (customer) {
         break;
     }
 
-    //add frequent renter points
     frequentRenterPoints++;
-    // add bonus for a two day new release rental
     if (movie.code === "lançamento" && r.days > 2) frequentRenterPoints++;
 
-    //print figures for this rental
     result += `\t${movie.title}\t${thisAmount}\n`;
     totalAmount += thisAmount;
   }
-  // add footer lines
+
   result += `Amount owed is ${totalAmount}\n`;
   result += `You earned ${frequentRenterPoints} frequent renter points\n`;
 
@@ -107,5 +100,78 @@ class Aluguel {
 
   calcularcustoExtra() {}
 }
+
+
+class Categoria {
+  #nome;
+  #custoBase;
+  #diasGratis;
+  constructor(nome) {
+    this.#nome = nome;
+    this.#custoBase = CUSTO_BASE_FILME.LANCAMENTO;
+    this.diasGratis = DIAS_GRATIS_CATEGORIA.LANCAMENTO;
+
+  }
+
+  get nome() {
+    return this.#nome;
+  }
+
+  set nome(novoNome) {
+    this.#nome = novoNome;
+  }
+
+
+  get custoBase() {
+    return this.#custoBase;
+  }
+
+  set custoBase(novoCustoBase) {
+    this.#custoBase = novoCustoBase;
+  }
+
+
+  get diasGratis() {
+    return this.#diasGratis;
+  }
+
+  set diasGratis(novosDiasGratis) {
+    this.#diasGratis = novosDiasGratis;
+  }
+
+
+}
+
+
+class Cliente{
+  #nome;
+  pontosPorLocacao = 0;
+
+  constructor(nome){
+    this.#nome = nome;
+
+  }
+
+  get nome() {
+    return this.#nome;
+  }
+
+  setNome(novoNome) {
+    this.#nome = novoNome;
+  }
+ pontosPorLocacao(){
+
+ }
+
+ pontosPorLocacao(filme, dias) {
+  this.pontosPorLocacao++;
+
+  if (filme.categoria === "LANCAMENTO" && dias > 2) {
+    this.pontosPorLocacao++;
+  }
+}
+}
+
+
 
 const matrix = new Movie("Matrix");
